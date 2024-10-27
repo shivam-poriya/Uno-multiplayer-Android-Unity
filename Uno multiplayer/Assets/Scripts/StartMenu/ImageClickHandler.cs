ee4861ecd278a1bb0df38855736d1f7f5a8eda97
shivam-poriya <shivamporiya@outlook.com>
2024-10-27 12:16:53 +0530

    Added Lobby UI
    Defined flow of UI
    Added basic UI for creating and joining lobby
    Added function of storing user name
    Used scriptable object and textmeshPro input field for storing user name

--- Uno multiplayer/Assets/Scripts/StartMenu/ImageClickHandler.cs
+++ Uno multiplayer/Assets/Scripts/StartMenu/ImageClickHandler.cs
@@ -1,10 +1,76 @@
+using System;
+using Unity.Services.Lobbies.Models;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class ImageClickHandler : MonoBehaviour, IPointerClickHandler
 {
+    [SerializeField] GameObject lobbyBack;
+    UserName userName;
+    [SerializeField] GameObject inputFieldUI;
+
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log("Image clicked!");
+        DisableText();
+        DisableRaycastOnImage();
+        userName = Resources.Load<UserName>("UserName");
+        if (userName.Name.Equals(""))
+            inputUserName();
+        else
+            EnableFirstLobbyInterface();
+
+    }
+
+    
+    void inputUserName()
+    {
+        inputFieldUI.SetActive(true);        
+    }
+
+    void DisableText()
+    {
+        /*
+        This function just disables the floating text which says "Click Anywhere to Start"
+        */
+        try
+        {
+            GameObject floatingText = GameObject.Find("Floating Text");
+            floatingText.SetActive(false);
+        }
+        catch(Exception e) 
+        {
+            Debug.LogError(e);
+        }
+    }
+
+    void DisableRaycastOnImage()
+    {
+        /*
+          * This function disables the raycast target of the image
+        */
+        try
+        {
+            Image startImage = GameObject.Find("Start Image").GetComponent<Image>();
+            startImage.raycastTarget = false;
+        }
+        catch (Exception e)
+        {
+            Debug.LogError(e);
+        }
+    }
+
+    public void EnableFirstLobbyInterface()
+    {
+        try
+        {
+            lobbyBack.SetActive(true);
+        }
+        catch(Exception e)
+        {
+            Debug.Log(e);
+        }
     }
 }
